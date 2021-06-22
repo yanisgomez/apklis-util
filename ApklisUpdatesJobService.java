b991ee52af50722aeb8960234af07b3e3b815ea8
yanisgomez <86133167+yanisgomez@users.noreply.github.com>
2021-06-22 04:15:58 -0400

    Add files via upload

---
+++ ApklisUpdatesJobService.java
@@ -0,0 +1,207 @@
+package your.app.package_id;
+
+import android.app.ActivityManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.LocalBroadcastManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.List;
+
+public class ApklisUpdatesJobService extends JobService {
+
+    private Handler ApklisUpdateServiceHandler;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        ApklisUpdateServiceHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what){
+                    case 1:
+                        boolean update_exist = msg.getData().getBoolean("update_exist",false);
+                        String version_name =  msg.getData().getString("version_name","");
+                        JobParameters jobParameters = msg.getData().getParcelable("params");
+
+                        boolean AppActive = false;
+                        ActivityManager am = (ActivityManager) getApplicationContext().getSystemService(getApplicationContext().ACTIVITY_SERVICE);
+                        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT_WATCH){
+
+                            List<ActivityManager.RunningAppProcessInfo> runningAppProcessInfoList = am.getRunningAppProcesses();
+                            for(ActivityManager.RunningAppProcessInfo processInfo : runningAppProcessInfoList){
+                                if(processInfo.importance==ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                                    for(String activeProcess : processInfo.pkgList){
+                                        if(activeProcess.equals(getApplicationContext().getPackageName())){
+                                            AppActive = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if(update_exist) {
+                            if (!AppActive) {
+
+                                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O) {
+                                    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                                    NotificationChannel chanel = new NotificationChannel("chanel","ApklisUpdate",NotificationManager.IMPORTANCE_DEFAULT);
+                                    notificationManager.createNotificationChannel(chanel);
+                                }
+
+                                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext()),"chanel")
+                                        .setSmallIcon(R.mipmap.ic_launcher)
+                                        .setContentTitle(getApplicationContext().getApplicationInfo().loadLabel(getApplicationContext().getPackageManager()).toString())
+                                        .setContentText("Nueva Versión v"+version_name+" Disponible En Apklis")
+                                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+                                notificationManagerCompat.notify(0, builder.build());
+                            }
+                            else {
+                                Intent intent = new Intent("apklis_update");
+                                intent.putExtra("version_name",version_name);
+                                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+                            }
+                        }
+
+                        jobFinished(jobParameters,false);
+
+
+                    default:super.handleMessage(msg);
+                }
+
+            }
+        };
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+
+        String APP_PACKAGE = params.getExtras().getString("APP_PACKAGE","");
+        NetWorkThread netWorkThread = new NetWorkThread(APP_PACKAGE,params);
+        new Thread(netWorkThread).start();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+
+
+class NetWorkThread extends Thread {
+
+    String APP_PACKAGE;
+    JobParameters jobParameters;
+
+    NetWorkThread(String APP_PACKAGE, JobParameters jobParameters) {
+        this.APP_PACKAGE = APP_PACKAGE;
+        this.jobParameters = jobParameters;
+    }
+
+    @Override
+    public void run() {
+
+        String api_apklis_json = "";
+        boolean update_exist = false;
+        int apklis_version_code = 0;
+        String apklis_version_name = "";
+
+        PackageInfo pinfo = null;
+        try {
+            pinfo = getApplicationContext().getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        int this_version_code = pinfo.versionCode;
+
+        android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+
+        URL url = null;
+        try {
+            url = new URL("https://api.apklis.cu/v1/application/?package_name=" + APP_PACKAGE);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+                BufferedReader api_apklis = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                String inputLine;
+                while ((inputLine = api_apklis.readLine()) != null) {
+                    api_apklis_json = api_apklis_json + inputLine;
+                }
+
+        }catch (UnknownHostException e){
+
+        }
+        catch (SocketException se){
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        if(!api_apklis_json.equals("")) {
+            String apklis_version_code_tem = "";
+            String version_code = "\"version_code\":";
+            int cd_Index = api_apklis_json.indexOf(version_code);
+            if(cd_Index!=-1) {
+                String code = String.valueOf(api_apklis_json.charAt(cd_Index + version_code.length()));
+                for (int i = version_code.length(); !code.equals(","); i++) {
+                    apklis_version_code_tem = apklis_version_code_tem + code;
+                    code = String.valueOf(api_apklis_json.charAt(cd_Index + i + 1));
+                }
+            } else apklis_version_code_tem = "";
+            if(!apklis_version_code_tem.equals("")) {
+                apklis_version_code = Integer.parseInt(apklis_version_code_tem);
+
+
+                if (apklis_version_code > this_version_code) {
+                    update_exist = true;
+
+                    int vn_index = api_apklis_json.indexOf("\"version_name\":");
+                    String vname = String.valueOf(api_apklis_json.charAt(vn_index +16));
+                    for (int i = 17; !vname.equals("\""); i++) {
+                        apklis_version_name = apklis_version_name + vname;
+                        vname = String.valueOf(api_apklis_json.charAt(vn_index + i));
+                    }
+                }
+            }
+
+        }
+
+        Message message = new Message();
+
+        message.what = 1;
+        Bundle bundle = new Bundle();
+
+        bundle.putParcelable("params", jobParameters);
+        bundle.putBoolean("update_exist", update_exist);
+        bundle.putString("version_name", apklis_version_name);
+        message.setData(bundle);
+        ApklisUpdateServiceHandler.sendMessage(message);
+
+    }
+}
+}
---
+++ ApklisUtil.java
@@ -0,0 +1,205 @@
+package your.app.package_id;
+
+import android.app.Activity;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.PersistableBundle;
+import android.os.Process;
+import android.os.RemoteException;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
+public class ApklisUtil extends Activity{
+
+    public final String DOWNLOADS = "\"download_count\":";
+    public final String SALES = "\"sale_count\":";
+    public final String RATING = "\"rating\":";
+    public final String PRICE = "\"price\":";
+    public final String REVIEWS = "\"reviews_count\":";
+    public final String REVIEWS_STAR_1 = "\"reviews_star_1\":";
+    public final String REVIEWS_STAR_2 = "\"reviews_star_2\":";
+    public final String REVIEWS_STAR_3 = "\"reviews_star_3\":";
+    public final String REVIEWS_STAR_4 = "\"reviews_star_4\":";
+    public final String REVIEWS_STAR_5 = "\"reviews_star_5\":";
+
+    Context CONTEXT;
+    String APP_PACKAGE;
+
+    public ApklisUtil(final Context CONTEXT, String APP_PACKAGE) {
+
+        this.CONTEXT = CONTEXT;
+        this.APP_PACKAGE = APP_PACKAGE;
+
+    }
+
+
+    public void startLookingForUpdates(int SECONDS){
+
+        PersistableBundle bundle = new PersistableBundle();
+        bundle.putString("APP_PACKAGE",APP_PACKAGE);
+
+        JobScheduler check_for_updates = (JobScheduler) CONTEXT.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo check_for_updates_job = new JobInfo.Builder(0,new ComponentName(CONTEXT, ApklisUpdatesJobService.class))
+                .setMinimumLatency(TimeUnit.SECONDS.toMillis(SECONDS))
+                .setExtras(bundle)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .build();
+        check_for_updates.schedule(check_for_updates_job);
+
+    }
+
+    public void startLookingForUpdates(int MINUTES, boolean PERIODIC){
+
+        PersistableBundle bundle = new PersistableBundle();
+        bundle.putString("APP_PACKAGE",APP_PACKAGE);
+
+        JobScheduler check_for_updates = (JobScheduler) CONTEXT.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo check_for_updates_job = new JobInfo.Builder(0,new ComponentName(CONTEXT, ApklisUpdatesJobService.class))
+                .setPeriodic(TimeUnit.MINUTES.toMillis(MINUTES))
+                .setExtras(bundle)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .build();
+        check_for_updates.schedule(check_for_updates_job);
+
+    }
+
+    public boolean checkPaymentApp() {
+        boolean paid = false;
+        Uri provider_URI = Uri.parse("content://cu.uci.android.apklis.payment.provider/app/" + APP_PACKAGE);
+        try {
+
+            ContentProviderClient contentResolver = CONTEXT.getContentResolver().acquireContentProviderClient(provider_URI);
+
+            if (contentResolver != null) {
+                Cursor apklisdb = contentResolver.query(provider_URI, null, null, null, null);
+                if (apklisdb.moveToFirst()) {
+                    paid = apklisdb.getInt(apklisdb.getColumnIndex("paid")) > 0;
+                }
+                apklisdb.close();
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    contentResolver.close();
+                } else {
+                    contentResolver.release();
+                }
+            }
+        } catch (RemoteException e1) {
+            e1.printStackTrace();
+        }
+
+        return paid;
+    }
+
+    public String getUserName() {
+
+        String username = null;
+        Uri provider_URI = Uri.parse("content://cu.uci.android.apklis.payment.provider/app/" + APP_PACKAGE);
+        try {
+
+            ContentProviderClient contentResolver = CONTEXT.getContentResolver().acquireContentProviderClient(provider_URI);
+
+            if (contentResolver != null) {
+                Cursor apklisdb = contentResolver.query(provider_URI, null, null, null, null);
+                if (apklisdb.moveToFirst()) {
+                    username = apklisdb.getString(apklisdb.getColumnIndex("user_name"));
+                }
+                apklisdb.close();
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    contentResolver.close();
+                } else {
+                    contentResolver.release();
+                }
+            }
+        } catch (RemoteException e1) {
+            e1.printStackTrace();
+        }
+
+        return username;
+    }
+
+    public void getInfo(String option){
+
+        NetWorkThreadInfo netWorkThread = new NetWorkThreadInfo(option);
+        new Thread(netWorkThread).start();
+
+    }
+
+
+    class NetWorkThreadInfo extends Thread{
+
+        String option;
+
+        public NetWorkThreadInfo(String option){
+
+            this.option = option;
+
+        }
+
+        @Override
+        public void run() {
+            String api_apklis_json = "";
+            float value = -1;
+            android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            if (option.equals("\"download_count\":") || option.equals("\"sale_count\":") || option.equals("\"rating\":")
+                    || option.equals("\"price\":") || option.equals("\"reviews_count\":") || option.equals("\"reviews_star_1\":")
+                    || option.equals("\"reviews_star_2\":") || option.equals("\"reviews_star_3\":") || option.equals("\"reviews_star_4\":")
+                    || option.equals("\"reviews_star_5\":")) {
+                URL url = null;
+                try {
+                    url = new URL("https://api.apklis.cu/v1/application/?package_name=" + APP_PACKAGE);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    BufferedReader api_apklis = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                    String inputLine;
+                    while ((inputLine = api_apklis.readLine()) != null) {
+                        api_apklis_json = api_apklis_json + inputLine;
+                    }
+
+                } catch (UnknownHostException e) {
+
+                } catch (SocketException se) {
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (!api_apklis_json.equals("")) {
+                    String string_value = "";
+                    int vn_index = api_apklis_json.indexOf(option);
+                    String value_count = String.valueOf(api_apklis_json.charAt(vn_index + option.length()));
+                    for (int i = option.length() + 1; !value_count.equals(","); i++) {
+                        string_value = string_value + value_count;
+                        value_count = String.valueOf(api_apklis_json.charAt(vn_index + i));
+                    }
+                    Log.e("Main", string_value);
+                    value = Float.parseFloat(string_value);
+
+                    Intent intent = new Intent("apklis_app_info");
+                    intent.putExtra("info_value",value);
+                    LocalBroadcastManager.getInstance(CONTEXT).sendBroadcast(intent);
+                }
+
+            }
+        }
+
+    }
+
+}
+
